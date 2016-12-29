44681d9c489fbd7e97eedbbd4578f41956e45fae
Andrey <Andrey>
2016-12-29 16:11:41 +0300

    ~

--- init2.sh
+++ init2.sh
@@ -1,3 +1,6 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo /etc/init.d/nginx stop
-sudo /usr/sbin/nginx -c /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /database/projects/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo rm -rf /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
+sudo ln -sf /database/projects/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
+sudo /etc/init.d/mysql start
