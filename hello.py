80e7894cc3870b2e1402dec9a97fe0d7f7f8a772
Andrey <Andrey>
2016-12-29 16:00:01 +0300

    ~

--- hello.py
+++ hello.py
@@ -3,18 +3,18 @@ Simple wsgi script
 '''
 
 
-def app(environ, start_response):
+def application(environ, start_response):
     '''
     simple wsgi function
     '''
-    
+
     status = "200 OK"
 
     headers = [
         ('Content-Type', 'text/plain'),
     ]
 
-    body = 'Hello World'
+    body = environ['QUERY_STRING']
 
     start_response(status, headers)
 
