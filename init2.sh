7ccddda428942341720a746d2e1ec397bc059519
Andrey <Andrey>
2016-12-29 16:24:53 +0300

    ~

--- init2.sh
+++ init2.sh
@@ -1,6 +1,5 @@
 sudo ln -sf /database/projects/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /database/projects/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start
