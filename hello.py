13be801b7ddb6e393e616252a0768b815d83bb2a
Andrey <Andrey>
2016-12-29 16:47:02 +0300

    ~

--- hello.py
+++ hello.py
@@ -14,7 +14,7 @@ def application(environ, start_response):
         ('Content-Type', 'text/plain'),
     ]
 
-    body = "Helloo" #environ['QUERY_STRING']
+    body = environ['QUERY_STRING']
 
     start_response(status, headers)
 
